7d9ff8068131a2822d23a0b1fe8eba272a442fd1
mateusfiorii <matheus.fiore87@gmail.com>
2020-06-14 21:48:56 -0300

    Project Finally

---
+++ Todo/Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAll("mateusfiori");
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllDone("mateusfiori");
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllUndone("mateusfiori");
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("mateusfiori", DateTime.Now.Date, true);
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("mateusfiori", DateTime.Now.Date, false);
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("mateusfiori", DateTime.Now.Date.AddDays(1), true);
+        }
+
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("mateusfiori", DateTime.Now.Date.AddDays(1), false);
+        }
+
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = "mateusfiori";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = "mateusfiori";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MaskAsDone([FromBody] MarkTodoAsDoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = "mateusfiori";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone([FromBody] MarkTodoAsUndoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = "mateusfiori";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+    }
+
+}
--- Todo/Todo.Domain.Api/Startup.cs
+++ Todo/Todo.Domain.Api/Startup.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Api
 {
@@ -21,25 +21,58 @@ namespace Todo.Domain.Api
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
+
+            // PROJETO NO FIREBASE 
+            // var firebaseConfig = {
+            //       apiKey: "AIzaSyAkKRPAfa7aToh20f1otCUIqGzH2xVxb5c",
+            //       authDomain: "todo-21c61.firebaseapp.com",
+            //       databaseURL: "https://todo-21c61.firebaseio.com",
+            //       projectId: "todo-21c61",
+            //       storageBucket: "todo-21c61.appspot.com",
+            //       messagingSenderId: "557907870209",
+            //       appId: "1:557907870209:web:24f7c758af2aeefa9db600"
+            //     };
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://securetoken.google.com/todo-21c61";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/todo-21c61",
+                        ValidateAudience = true,
+                        ValidAudience = "todo-21c61",
+                        ValidateLifetime = true
+                    };
+                });
+
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
